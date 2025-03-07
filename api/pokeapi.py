8d5110c92515a86abc43533cf1a11306b79050ae
mengelhardt21 <36308565+mengelhardt21@users.noreply.github.com>
2025-03-07 00:22:44 -0500

    Made first call.

--- api/pokeapi.py
+++ api/pokeapi.py
@@ -2,14 +2,21 @@ import requests
 from requests import *
 import json
 import pprint
-import urllib.parse
-import sys
 from pathlib import Path
 
-base_url = 'https://pokeapi.co/api/v2/'
+base_url = 'https://pokeapi.co/api/v2'
 
 def get_pokemon (id):
 
-    url = f"{base_url}/pokemon/{id}"
+    url = f"{base_url}/pokemon/{id}/"
 
-    
+    response = requests.get(url)
+
+    pprint.pprint(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        pprint.pprint(f'Error: {response.status_code}')
+
+pprint.pprint(get_pokemon(1))
---
+++ main.py
@@ -0,0 +1,5 @@
+import click
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"/api")))
+from pokeapi import pokeapi
